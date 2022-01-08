20f71b03e574f687cb0fd7e05cae039d25a3a973
awkshwayrd <mathur.aksh@gmail.com>
2022-01-08 16:10:37 +0530

    reactive-mysql-with-jasync-and-r2dbc

---
+++ reactive-mysql-with-jasync-and-r2dbc/src/test/java/com/reactive/reactivemysqlwithjasyncandr2dbc/ReactiveMysqlWithJasyncAndR2dbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactive.reactivemysqlwithjasyncandr2dbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveMysqlWithJasyncAndR2dbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
